0dda5341d6adda26b076bd583531a2529f3d9cde
AI1411 <goethe0719@gmail.com>
2022-02-07 22:14:07 +0900

    listAccount追加

--- api/account.go
+++ api/account.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	db "github.com/AI1411/golang-postgres-k8s/db/sqlc"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -14,6 +15,11 @@ type getAccountRequest struct {
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,oneof=USD,EUR,JPY"`
+}
+
+type listAccountRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
@@ -43,5 +49,22 @@ func (server *Server) createAccount(ctx *gin.Context) {
 }
 
 func (server *Server) listAccount(ctx *gin.Context) {
+	var req listAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
 }
